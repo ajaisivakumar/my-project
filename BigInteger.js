801f28a4c2b983baae132c21d824dfc35bb188aa
AjaiSivakumar <ajaipc405@example.com>
2026-02-17 00:37:09 +0530

    intial commit

---
+++ BigInteger.js
@@ -0,0 +1,65 @@
+// // let peru  = "HarryPotter";
+// // console.log(peru);
+// // peru = " ajia";
+// // console.log(peru);
+// // const ajai = 435
+// // console.log(ajai);
+// // ajai = 5333;
+// // console.log(ajai);
+// // let BigInteger = require('big-integer');
+// // let ak = 3425345354343563635345353543636345345334n;
+// // console.log(ak);
+// // let nam = "jai ";
+// // let sentence = "`I am a software engineer  ${nam}`";
+// // console.log(sentence);
+// let ajith = " ";
+// console.log(ajith);
+// let val;
+// console.log(val);
+// let a = "atrungjijgsfs";
+// console.log(typeof a );
+// a = Number(a);
+// console.log(typeof a);
+// console.log(a);
+// console.log (${5355}:)
+// console.log(~10);
+// let a = b = c = 34;
+// console.log(a,b,c);
+// let a = 23;
+// let b = 4;
+// console.log(++a + b++);
+// console.log(undefined<= 0);
+// console.log(undefined==0);
+// console.log(undefined < 0);
+// let  a = null ;
+// a = Number(a);
+// console.log(a);
+
+
+
+// let age = 18;
+// let vote = (age >= 18)? "elgible" : "noteligible";
+// console.log(vote);
+
+// let  a = 3;
+// let  b  =3;
+// let  c = 3;
+// let max = (a>b)? (a>c? a : c):(b>c? b : c);
+// console.log(max);
+
+
+// console.log(null||0||undefined );
+// let a = ``;
+// let b = a ?? 20;
+// console.log(b);
+let a = 20 ;
+
+let checkodd =  a =>{
+    if(a % 2 === 0){
+        return "even";
+    }
+    else{
+        return "odd";
+    }
+}
+console.log(checkodd(a));
---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+let car= {
+    name : "tesla",
+    model : " ev324",
+    price : 3242,
+    "year" : 2025,
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// bike.name = "hero splendor";
+// console.log(car);
+// console.log(bike);
